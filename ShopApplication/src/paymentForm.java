49737aa3575310a481525b553b4cedd8b1e1e48e
Jasper Cox <57488312+Jasper-27@users.noreply.github.com>
2020-12-07 22:16:49 +0000

    payment form is now runnable

--- ShopApplication/src/paymentForm.java
+++ ShopApplication/src/paymentForm.java
@@ -4,6 +4,20 @@ public class paymentForm {
     private JButton btn_cardPay;
     private JButton btn_cashPay;
     private JTextField tf_cashAmount;
+    private JPanel pnl_payment;
+
+    public static void main(String[] args) {
 
 
+        JFrame frame = new JFrame("receiptWindow");
+        frame.setContentPane(new paymentForm().pnl_payment);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        //frame.setSize(500, 400);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+
+
+    }
 }
